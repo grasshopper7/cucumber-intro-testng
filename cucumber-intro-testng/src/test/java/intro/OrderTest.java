15341f72504b1ed03bcdadf3b592f6eb0e848251
grasshopper7 <grass.hopper.moc@gmail.com>
2019-09-26 10:26:08 +0530

    Add summary and progress plugins

--- cucumber-intro-testng/src/test/java/intro/OrderTest.java
+++ cucumber-intro-testng/src/test/java/intro/OrderTest.java
@@ -1,5 +1,7 @@
 package intro;
 
+import io.cucumber.testng.CucumberOptions;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 
+@CucumberOptions(plugin = { "summary", "progress" })
 public class OrderTest extends AbstractTestNGCucumberTests{}
